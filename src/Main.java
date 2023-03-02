a616a10884fd1777078e8691379a0db61d863c1e
Gabriel Mazzotti <gmazzottidesouza@gmail.com>
2023-03-02 17:38:31 -0300

    Mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("Descrição do curso");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso java");
+        curso2.setDescricao("Descrição do curso");
+        curso2.setCargaHoraria(2);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria java");
+        mentoria.setDescricao("Descrição da mentoria");
+        mentoria.setData(LocalDate.now());
     }
+
 }
